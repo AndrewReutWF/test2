67c9a9319795750c76ec0666aad29016217011ee
AndrewReut <areut@workfusion.com>
2023-11-13 19:37:37 +0100

    Second

--- src/com/teachmeskills/oleg/Test.java
+++ src/com/teachmeskills/oleg/Test.java
@@ -3,7 +3,7 @@ package com.teachmeskills.oleg;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
     }
 
 }
